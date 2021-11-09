1cd8d5086042d54330fd1b338b92bf40ff0ddf43
Trexxin <Charlesharris2322@gmail.com>
2021-11-09 15:11:01 -0600

    Base Files

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext.commands import bot
+
+client = discord.Client()
+
+#Confirms the bot is online
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$alex'):
+        await message.channel.send('Alex is a bitch!')
+
+@client.event   #Event to detect when a user joins the server
+async def on_member_join(member):
+    await member.send('Hi! Welcome to our server, in 30 seconds you will get "Noob" role!')
+    verifiedRole = discord.utils.get(member.guild.roles, id=842062523041185802)
+    await member.add_roles(verifiedRole)
+
+client.run('OTA3NTE2MTY5NTI1ODIxNDUw.YYoUSw.k6WgS43MSJz4y5XH6lxoVLL4McY')
+
